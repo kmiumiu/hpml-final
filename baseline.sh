968bda68668ca58f83f60ecaa55c863eb207eb44
kmiumiu <km3635@columbia.edu>
2024-12-15 12:19:37 -0500

    edit baseline experiment script (2 gpu)

--- baseline.sh
+++ baseline.sh
@@ -8,14 +8,14 @@
 #SBATCH --time=30:00              # The time the job will take to run (here, 1 min)
 #SBATCH --mem-per-cpu=1gb        # The memory the job will use per cpu core.
  
-torchrun --nproc_per_node=1 --master_port=1234 train.py \
+torchrun --nproc_per_node=2 --master_port=1234 train.py \
     --model_name_or_path "meta-llama/Llama-3.2-1B-Instruct" \
     --data_path ./alpaca_data.json \
     --fp16 True \
-    --output_dir "./results" \
+    --output_dir "./baseline" \
     --num_train_epochs 3 \
-    --per_device_train_batch_size 1 \
-    --per_device_eval_batch_size 1 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 8 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
@@ -26,4 +26,6 @@ torchrun --nproc_per_node=1 --master_port=1234 train.py \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
+    --fsdp "full_shard auto_wrap" \
+    --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' 
 # End of script
