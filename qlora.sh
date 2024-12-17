23037ee5c4c614b5f56e951d99fae30d31ff0ac2
Ken Miura <km3635@columbia.edu>
2024-12-17 16:33:39 -0500

    add qlora

---
+++ qlora.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+#
+#SBATCH --account=edu      # The account name for the job.
+#SBATCH --job-name=Baseline    # The job name.
+#SBATCH --gres=gpu:2
+#SBATCH -c 12                    # The number of cpu cores to use.
+#SBATCH --time=9:30:00              # The time the job will take to run (here, 1 min)
+#SBATCH --mem-per-cpu=8gb        # The memory the job will use per cpu core.
+ 
+torchrun --nproc_per_node=2 --master_port=1234 train_qlora.py \
+    --model_name_or_path "meta-llama/Llama-3.2-1B-Instruct" \
+    --data_path ./alpaca_data.json \
+    --fp16 True \
+    --output_dir "./qlora" \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 32 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 2000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --fsdp "full_shard auto_wrap" \
+    --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' 
+# End of script
